a49471b9a1e50af88653d196de2a966348d593e0
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-08-06 15:29:20 +0200

    Update and rename Hompage.jsx to Homepage.jsx

--- app/src/Components/Homepage.jsx
+++ app/src/Components/Homepage.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import Dashboard from '../../Components/dashcomponents/Dashboard'
+
 import Footer from '../../Components/Common/Footer';
 import Navbar from '../../Components/Common/DashNav';
 
 
-function DashboardPage() {
+function Homepage() {
   return (
     <>
     <Navbar />
-    <Dashboard />
+   
     <Footer />
     </>
   )
 }
 
-export default DashboardPage
+export default Homepage
