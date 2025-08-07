8a7e11fba28d50e119670d635a205ae1ea05829b
Andrea Vicari <andrea.vicari77@gmail.com>
2025-08-07 14:07:08 +0200

    afagf

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import { Routes, Route, Navigate} from 'react-router';
-import ScrollToTop from './Components/ScrollToTop';
-import Homepage from './Components/Homepage';
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route} from 'react-router-dom';
 
+import NavbarFixedTop from './Components/Common/NavbarFixedTop';
+import Footer from './Components/Common/Footer';
+import ScrollToTop from './Components/ScrollToTop';
+import HomepageOne from './pages/HomepageOne';
+import AboutUsPage from './pages/AboutUsPage';
+import ServicePage from './pages/Servicepage';
+import ContactPage from './pages/ContactPage';
+import BlogPage from './pages/BlogPage';
+import ProjectsPage from './pages/ProjectsPage';
+import PrivacyPage from './pages/PrivacyPage';
+import FaqPage from './pages/FaqPage';
+import SinglePost from './pages/SinglePost';
+import SingleProject from './pages/SingleProject';
+import Error404 from './pages/Error404';
 
 function App() {
 
-
-
-
   return (
     <React.Fragment>
+      <NavbarFixedTop />
       <ScrollToTop>
         <Routes>
-        <Route path="/" element={<Homepage />} />
+            <Route path="/" element={<HomepageOne />} />
+            <Route path="/aboutus" element={<AboutUsPage />} />
+            <Route path="/services" element={<ServicePage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/blogpage" element={<BlogPage />} />
+            <Route path="/faqpage" element={<FaqPage />} />
+            <Route path="/privacy" element={<PrivacyPage />} />
+            <Route path="/projectspage" element={<ProjectsPage />} />
+            <Route path="/singlepost" element={<SinglePost />} />
+            <Route path="/singleproject" element={<SingleProject />} />
+            <Route path="*" element={<Error404 />} />
 
         </Routes>
         </ScrollToTop>
-
+      <Footer />
     </React.Fragment>
 
   )
