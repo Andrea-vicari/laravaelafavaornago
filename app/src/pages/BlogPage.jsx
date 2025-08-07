14a0280d060c452641d877d7ee789a63576b46ce
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-08-07 14:43:08 +0200

    Update BlogPage.jsx

--- app/src/pages/BlogPage.jsx
+++ app/src/pages/BlogPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router';
 import { useSelector } from 'react-redux'
 import newsList from '../../src/assets/news/news.json';
 
